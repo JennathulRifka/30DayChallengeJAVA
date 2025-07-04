6eb00af383b6baff30554762711c2ddd9cc9ba66
mnjrifka <riekiee33@gmail.com>
2025-07-04 22:57:20 +0530

    Day 06
    Palindrome

---
+++ Day06_Palindrome/Palindrome/src/main/java/com/mycompany/palindrome/Palindrome.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.palindrome;
+import java.util.Scanner;
+
+/**
+ *
+ * @author SINGER
+ */
+public class Palindrome {
+    
+    
+
+    public static void main(String[] args) {
+        
+Scanner scanner = new Scanner(System.in);
+
+        // Ask user for input
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        // Clean the input (optional: remove spaces and make lowercase)
+        String cleaned = input.replaceAll("\\s+", "").toLowerCase();
+
+        // Reverse the string
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        // Check for palindrome
+        if (cleaned.equals(reversed)) {
+            System.out.println("The input is a palindrome.");
+        } else {
+            System.out.println("The input is not a palindrome.");
+        }
+        
+    }
+}
