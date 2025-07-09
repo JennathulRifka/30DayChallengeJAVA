f0dc40a2569ed8a2fc70f92b090564dc74b95800
mnjrifka <riekiee33@gmail.com>
2025-07-09 11:57:01 +0530

    Day 11
    Polymorphism

---
+++ Day11_polymorphism/Animal/src/main/java/com/mycompany/animal/Dog.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.animal;
+
+/**
+ *
+ * @author SINGER
+ */
+public class Dog extends Animal{
+    @Override
+    public void animalSound(){
+        System.out.println("Dog says: Woof Woof");
+    }
+    
+}
